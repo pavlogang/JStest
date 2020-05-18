c60c38ed2768c4ad343c426b68056fe3db915430
Pavel Baranov <pb@smarthead.ru>
2020-05-18 19:50:22 +0300

    +

--- app.js
+++ app.js
@@ -5,46 +5,46 @@
 let money = +prompt('Ваш бюджет на месяц?', '');
 let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let a3 = prompt('Введите обязательную статью расходов в это месяце?','');
-let a4 = prompt('Во сколько обойдется?','');
+let state = prompt('Введите обязательную статью расходов в это месяце?');
+console.log(state);
+
+let price = prompt('Во сколько обойдется?');
 
 let appData = {
     money: money,
     timeData: time,
-    expenses: {},
+    expenses: {
+        state: price
+    },
     optionalExpenses: {},
     income: [],
     savingis: false
 };
 
 for (let i = 0; i < 2; i++) {
-    let a = prompt('Введите обязательную статью расходов в это месяце?',''),
-        b = prompt('Во сколько обойдется?','');
-
-        if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-         && a != '' && b != '' && a.length < 50 ) {
+        if ( (typeof(state)) === 'string' && (typeof(state)) != null && (typeof(price)) != null
+         && state != '' && price != '' && state.length < 50 ) {
             console.log('done');
-            appData.expenses[a] = b;
         } else {
-
+            console.log('Error');
+            
         }
-
-
 
 };
 
-        appData.moneyPerDay = appData.money / 30;
 
-        alert("Твои пожитки на день " + appData.moneyPerDay);
+    appData.moneyPerDay = appData.money / 30;
 
-        if(appData.moneyPerDay < 100) {
-            console.log('Вы бимж');
-        } else if ( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-            console.log('Можно и покушать');
-        }else if ( appData.moneyPerDay > 2000) {
-            console.log('Мажор ебатб');
-        } else {
-            console.log('Шо то поломато');
-        }
+    alert("Твои пожитки на день " + appData.moneyPerDay);
+
+    if(appData.moneyPerDay < 100) {
+        console.log('Вы бимж');
+    } else if ( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log('Можно и покушать');
+    }else if ( appData.moneyPerDay > 2000) {
+        console.log('Мажор ебатб');
+    } else {
+        console.log('Шо то поломато');
+    }
 
 
