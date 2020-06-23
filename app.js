2f63d48ca2f0886f04069117706c5a5e52b578d1
Pavel Baranov <pb@smarthead.ru>
2020-06-23 23:08:08 +0300

    создал объект с методами

--- app.js
+++ app.js
@@ -1,51 +1,78 @@
 /* -----------------------------------------------
 Первое задание
 */
+let money, time;
 
-let money = +prompt('Ваш бюджет на месяц?', '');
-let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
+function start() {
+    money = +prompt('Ваш бюджет на месяц?', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '2020-00-00');
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt('Ваш бюджет на месяц?', '');
+    }
+}
+
+start();
 
 let appData = {
     money: money,
     timeData: time,
     expenses: {
 
-     },
+    },
     optionalExpenses: {
 
     },
     income: [],
-    savingis: false
-};
+    savingis: true,
+    chooseExpenses: function () {
+        for (let i = 0; i < 2; i++) {
+            let state = prompt('Введите обязательную статью расходов в это месяце?');
+            let price = prompt('Во сколько обойдется?');
 
-for (let i = 0; i < 2; i++) {
-    let state = prompt('Введите обязательную статью расходов в это месяце?');
-    let price = prompt('Во сколько обойдется?');
-
-    if ( (typeof(state)) === 'string' && (typeof(state)) != null && (typeof(price)) != null
-         && state != '' && price != '' && state.length < 50 ) {
-            console.log('done');
-            appData.expenses[state] = price;
-        } else {
-            i -= 1;
+            if ((typeof (state)) === 'string' && (typeof (state)) != null && (typeof (price)) != null &&
+                state != '' && price != '' && state.length < 50) {
+                console.log('done');
+                appData.expenses[state] = price;
+            } else {
+                i -= 1;
+            }
         }
-
-};
-
-
-    appData.moneyPerDay = appData.money / 30;
-
-    alert("Твои пожитки на день " + parseInt(appData.moneyPerDay));
-
-    if(appData.moneyPerDay < 100) {
-        console.log('Вы бимж');
-    } else if ( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-        console.log('Можно и покушать');
-    }else if ( appData.moneyPerDay > 2000) {
-        console.log('Мажор ебатб');
-    } else {
-        console.log('Шо то поломато');
+    },
+    detectedDayBudget: () => {
+        appData.moneyPerDay = Math.trunc(appData.money / 30);
+        alert("Твои пожитки на день " + parseInt(appData.moneyPerDay));
+    },
+    detecteLevel: () => {
+        if (appData.moneyPerDay < 100) {
+            console.log('Вы бимж');
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log('Можно и покушать');
+        } else if (appData.moneyPerDay > 2000) {
+            console.log('Мажор ебатб');
+        } else {
+            console.log('Шо то поломато');
+        }
+    },
+    checkSavings: () => {
+        if (appData.savingis == true) {
+            let save = +prompt("Какова сумма накоплений?"),
+                percent = +prompt("Под какой процент?");
+            
+            appData.income = save / 100 / 12 * percent;
+            alert("Доход в месяц с вашего депозита:" + appData.income);
+        }
+    },
+    chooseOptExpenses: () => {
+        for (let i = 1; i < 3; i++) {
+            let opt = prompt("Статья необязатльных расходов?", "");
+            appData.optionalExpenses[i] = opt;
+        }
+    },
+    chooseIncome: () => {
+        let items = prompt ('Что принесет дополнительный доход? (Перечислите через запятую), ""')
+        appData.income = items.split(', ');
+        appData.income.push(prompt('Шо еще?'))ж
+        appData.income.sort();
     }
-
-
+    }
